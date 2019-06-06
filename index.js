7c7ef05d0f04f8bf53719dea7b5e09f8521557e0
Eren Dogan <eren.dogan@gaziulas.com.tr>
2019-06-06 07:15:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var customerName = 'bob';
 const leastFavoriteCustomer = 'ahmet';
 
 function upperCaseCustomerName (customerName) {
-  customerName.toUpperCase();
+  return customerName.toUpperCase();
 }
 
 function setBestCustomer() {
